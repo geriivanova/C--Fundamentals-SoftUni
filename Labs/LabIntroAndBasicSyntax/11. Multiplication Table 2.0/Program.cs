2c67b1422444932486b9cf939a361dd1d487aa0b
Gergana Ivanova <geri7423@abv.bg>
2024-02-05 20:52:55 +0200

    Upload LabIntroAndBasicSyntax

---
+++ Labs/LabIntroAndBasicSyntax/11. Multiplication Table 2.0/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace _11._Multiplication_Table_2._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+            int mul=int.Parse(Console.ReadLine());
+            if(mul>10)
+            {
+                Console.WriteLine($"{number} X {mul} = {number * mul}");
+            }
+            else
+            {
+                for (int i = mul;i<=10;i++)
+                {
+                    Console.WriteLine($"{number} X {i} = {number * i}");
+                }
+            }
+            
+        }
+    }
+}
