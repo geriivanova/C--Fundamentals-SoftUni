fde11105a7880a8e5ff1bbabb3719decd5e76110
Gergana Ivanova <geri7423@abv.bg>
2024-02-09 20:58:45 +0200

    add

---
+++ LabArrays/05. Sum Even Numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _05._Sum_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int sum = 0;
+            for(int i=0; i<nums.Length; i++)
+            {
+                int currentNum = nums[i];
+                if(currentNum%2 == 0)
+                {
+                     sum += currentNum;
+                }
+                
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
