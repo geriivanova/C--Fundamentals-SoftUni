c099c8efcb062347c38a0bd2e7b798fd60afbe20
Gergana Ivanova <geri7423@abv.bg>
2024-02-09 20:57:48 +0200

    add

---
+++ LabArrays/04. Reverse Array of Strings/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _04._Reverse_Array_of_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] strings = Console.ReadLine().Split(" ");
+            for (int i = strings.Length-1; i >= 0; i--)
+            {
+                Console.Write(strings[i] + " ");
+            }
+        }
+    }
+}
