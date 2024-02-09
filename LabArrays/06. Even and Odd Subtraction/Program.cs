0df257f9c45b4d074767401991e4a6dcf89d2ccc
Gergana Ivanova <geri7423@abv.bg>
2024-02-09 20:59:50 +0200

    add

---
+++ LabArrays/06. Even and Odd Subtraction/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _06._Even_and_Odd_Subtraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int sumOfEven = 0;
+            int sumOfOdd = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int currentNum = nums[i];
+                if (currentNum % 2 == 0)
+                {
+                    sumOfEven += currentNum;
+                }
+                else if(currentNum % 2 != 0)
+                {
+                    sumOfOdd += currentNum;
+                }
+            }
+            Console.WriteLine(sumOfEven-sumOfOdd);
+        }
+    }
+}
