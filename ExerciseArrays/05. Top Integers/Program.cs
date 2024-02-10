f4f8e087e0268f312eecba3a6b8facc2d655615c
Gergana Ivanova <geri7423@abv.bg>
2024-02-10 20:01:16 +0200

    new add

---
+++ ExerciseArrays/05. Top Integers/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace _05._Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            for(int i=0; i<numbers.Length; i++)
+            {
+                bool check = true;
+                for(int j=i+1; j<numbers.Length; j++)
+                {
+                    if (numbers[i] <= numbers[j])
+                    {
+                        check = false;
+                    }
+                }
+                if (check)
+                {
+                    Console.Write(numbers[i] + " ");
+                }
+                
+            }
+        }
+    }
+}
