f559a014bef6b9169c90ad5e8f28ca19f55b92b5
Gergana Ivanova <geri7423@abv.bg>
2024-02-10 20:00:10 +0200

    new add

---
+++ ExerciseArrays/03. Zig-Zag Arrays/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _03._Zig_Zag_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n= int.Parse(Console.ReadLine());
+            bool isFirstArratSelected = true;
+            string[] arr1 = new string[n];
+            string[] arr2 = new string[n];
+            for (int i = 0; i < n; i++)
+            {
+                string[] numbers = Console.ReadLine().Split();
+                if(isFirstArratSelected)
+                {
+                    arr1[i] = numbers[0];
+                    arr2[i] = numbers[1];
+                }
+                else
+                {
+                    arr1[i] = numbers[1];
+                    arr2[i] = numbers[0];
+                }
+                isFirstArratSelected = ! isFirstArratSelected;
+            }
+            Console.WriteLine(string.Join(" ",arr1));
+            Console.WriteLine(string.Join(" ", arr2));
+
+        }
+    }
+}
