2e95ec612314a3a1a52ebd4533eac2f1187540fd
Gergana Ivanova <geri7423@abv.bg>
2024-02-10 19:58:19 +0200

    new add

---
+++ ExerciseArrays/01. Train/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace _01._Train
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int[] wagon= new int[n];
+            for(int i = 0; i < n; i++)
+            {
+                int number=int.Parse(Console.ReadLine());
+                sum += number;
+                wagon[i] = number;
+            }
+            Console.WriteLine(string.Join(" ",wagon));
+            Console.WriteLine(sum);
+        }
+    }
+}
