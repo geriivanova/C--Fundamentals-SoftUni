14e93c3b266c737fcc11eb8446eebae3ace8e245
Gergana Ivanova <geri7423@abv.bg>
2024-02-11 10:49:00 +0200

    add

---
+++ LabMethods/06. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _06._Calculate_Rectangle_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());   
+            Console.WriteLine(GetRectangleArea(width, height));
+        }
+
+        static double GetRectangleArea(double width, double height)
+        {
+            return width * height;
+        }
+           
+    }
+}
