756af20706a463c28de64072e9339b1455bea58f
Gergana Ivanova <geri7423@abv.bg>
2024-02-11 10:49:53 +0200

    add

---
+++ LabMethods/08. Math Power/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _08._Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double number = double.Parse(Console.ReadLine());
+            double power = double.Parse(Console.ReadLine());
+            Console.WriteLine(RaiseToPower(number, power));
+        }
+
+        static double RaiseToPower(double n, double pow)
+        {
+            double rezult;
+            rezult = Math.Pow(n, pow);
+            return rezult;
+
+        }
+    }
+}
