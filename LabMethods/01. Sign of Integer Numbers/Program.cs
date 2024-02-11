94624eac7c1adeab068dd5978e017dc7dca141b1
Gergana Ivanova <geri7423@abv.bg>
2024-02-11 10:41:45 +0200

    add

---
+++ LabMethods/01. Sign of Integer Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01._Sign_of_Integer_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number=int.Parse(Console.ReadLine());
+
+            SignOfNumber(number);
+        }
+        static void SignOfNumber(int n)
+        {
+            if (n > 0)
+            {
+                Console.WriteLine($"The number {n} is positive.");
+            }
+            else if (n < 0)
+            {
+                Console.WriteLine($"The number {n} is negative.");
+            }
+            else if (n == 0)
+            {
+                Console.WriteLine($"The number {n} is zero.");
+            }
+        }
+    }
+}
