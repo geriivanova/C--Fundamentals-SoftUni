200f4cc7e3e5dbc359fa64bad906a46361af40a6
Gergana Ivanova <geri7423@abv.bg>
2024-02-11 10:48:06 +0200

    add

---
+++ LabMethods/04. Printing Triangle/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _04._Printing_Triangle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            PrintTriangle(number);
+        }
+
+        static void PrintLine(int start, int end)
+        {
+            for(int i = start; i <= end; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static void PrintTriangle(int n)
+        {
+            for(int i=1; i<=n; i++)
+            {
+                PrintLine(1,i);
+            }
+            for (int i = n-1; i >= 1; i--)
+            {
+                PrintLine(1, i);
+            }
+        }
+    }
+}
