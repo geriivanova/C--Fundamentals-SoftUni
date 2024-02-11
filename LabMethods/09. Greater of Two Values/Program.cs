60071b1ac0c40cb8033d21e774f664346150defd
Gergana Ivanova <geri7423@abv.bg>
2024-02-11 10:50:18 +0200

    add

---
+++ LabMethods/09. Greater of Two Values/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _09._Greater_of_Two_Values
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            string inputType = Console.ReadLine();
+            string firstInput = Console.ReadLine();
+            string secondInput = Console.ReadLine();
+
+            switch (inputType)
+            {
+                case "int":
+                    int firstInt = int.Parse(firstInput);
+                    int secondInt = int.Parse(secondInput);
+                    int resultInt = GetMax(firstInt, secondInt);
+                    Console.WriteLine(resultInt);
+                    break;
+                case "char":
+                    char firstChar = char.Parse(firstInput);
+                    char secondChar = char.Parse(secondInput);
+                    char resultChar = GetMax(firstChar, secondChar);
+                    Console.WriteLine(resultChar);
+                    break;
+                case "string":
+                    string firstString = firstInput;
+                    string secondString = secondInput;
+                    string resultString = GetMax(firstString, secondString);
+                    Console.WriteLine(resultString);
+                    break;
+            }
+        }
+
+        public static int GetMax(int first, int second)
+        {
+            return Math.Max(first, second);
+        }
+
+        public static char GetMax(char first, char second)
+        {
+            return (char)Math.Max(first, second);
+        }
+
+        public static string GetMax(string first, string second)
+        {
+            int comparison = first.CompareTo(second);
+
+            if (comparison > 0)
+            {
+                return first;
+            }
+            else
+            {
+                return second;
+            }
+        }
+    }
+}
