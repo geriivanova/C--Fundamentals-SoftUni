b0993c6b27550479c05dd1146840c7b09eabfef7
Gergana Ivanova <geri7423@abv.bg>
2024-02-11 10:49:25 +0200

    add

---
+++ LabMethods/07. Repeat String/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace _07._Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int count = int.Parse(Console.ReadLine());
+            Console.WriteLine(RepeatString(input, count));
+        }
+
+        static string RepeatString(string input, int count)
+        {
+            string rezult = "";
+
+            for (int i = 0; i < count; i++)
+            {
+                rezult += input;
+            }
+            return rezult;
+        }
+    }
+}
