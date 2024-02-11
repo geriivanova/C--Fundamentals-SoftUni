a96b6b2f93026fad57108a4dae0df447965f8f2e
Gergana Ivanova <geri7423@abv.bg>
2024-02-11 10:50:39 +0200

    add

---
+++ LabMethods/10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace _10._Multiply_Evens_by_Odds
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            int n = Math.Abs(int.Parse(Console.ReadLine()));
+            int result = GetMultipleOfEvenAndOdds(n);
+            Console.WriteLine(result);
+        }
+        static int GetMultipleOfEvenAndOdds(int n)
+        {
+            int sumEvens = GetSumOfEvenDigits(n);
+            int sumOdds = GetSumOfOddDigits(n);
+            return sumEvens * sumOdds;
+        }
+        static int GetSumOfEvenDigits(int n)
+        {
+            int sumEven = 0;
+            while (n > 0)
+            {
+                int lastDigit = n % 10;
+
+                if (lastDigit % 2 == 0) 
+                { 
+                    sumEven += lastDigit; 
+                }
+                n /= 10;
+            }
+            return sumEven;
+        }
+        static int GetSumOfOddDigits(int n)
+        {
+            int sumOdd = 0;
+            while (n > 0)
+            {
+                int lastDigit = n % 10;
+
+                if (lastDigit % 2 != 0) 
+                { 
+                    sumOdd += lastDigit; 
+                }
+                n /= 10;
+            }
+            return sumOdd;
+        }
+    }
+}
