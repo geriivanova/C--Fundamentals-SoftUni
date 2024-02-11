0654665304837901268ab03c93eea1554499587b
Gergana Ivanova <geri7423@abv.bg>
2024-02-11 10:48:32 +0200

    add

---
+++ LabMethods/05. Orders/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace _05._Orders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string product=Console.ReadLine();
+            int quantity = int.Parse(Console.ReadLine());
+            PriceOfProducts(product, quantity);
+        }
+
+        static void PriceOfProducts(string product, int quantity)
+        {
+            if (product == "coffee")
+            {
+                Console.WriteLine($"{ quantity * 1.50:F2}");
+            }
+            else if(product == "water")
+            {
+                Console.WriteLine($"{quantity * 1.00:F2}");
+            }
+            else if(product == "coke")
+            {
+                Console.WriteLine($"{quantity * 1.40:F2}");
+            }
+            else if(product== "snacks")
+            {
+                Console.WriteLine($"{quantity * 2.00:F2}");
+            }
+        }
+    }
+}
