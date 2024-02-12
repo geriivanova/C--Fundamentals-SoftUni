4fc580114329ff2ffb93126053b4207f4e5aa139
Gergana Ivanova <geri7423@abv.bg>
2024-02-12 23:46:11 +0200

    new

---
+++ ExerciseMethods/02. Vowels Count/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _02._Vowels_Count
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            int rezult = Vowels(word);
+
+            Console.WriteLine(rezult);
+
+        }
+
+        public static int Vowels(string word)
+        {
+            int count = 0;
+
+            word = word.ToLower();
+
+            foreach (var symbol in word)
+            {
+                if (symbol == 'a' || symbol == 'e' || symbol == 'o' || symbol == 'u' || symbol == 'i')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+    }
+}
