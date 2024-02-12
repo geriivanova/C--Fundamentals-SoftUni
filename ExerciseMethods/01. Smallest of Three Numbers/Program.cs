ee0eb0380ed6c4f1bf771c0f60dbdc6ac8384246
Gergana Ivanova <geri7423@abv.bg>
2024-02-12 23:45:27 +0200

    new

---
+++ ExerciseMethods/01. Smallest of Three Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01._Smallest_of_Three_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());    
+            GetSmallest(firstNumber,secondNumber,thirdNumber);
+        }
+
+        public static void GetSmallest(int a, int b, int c)
+        {
+            int minValue = a;
+            if(b < minValue)
+            {
+                minValue = b;
+            }
+            if(c < minValue)
+            {
+                minValue = c;
+            }
+            Console.WriteLine(minValue);
+        }
+    }
+}
