56cc15d5e843b9161ef81a944ebfea58a35f0420
Gergana Ivanova <geri7423@abv.bg>
2024-02-12 23:47:19 +0200

    new

---
+++ ExerciseMethods/03. Characters in Range/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _03._Characters_in_Range
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = char.Parse(Console.ReadLine());
+            char b = char.Parse(Console.ReadLine());
+
+            CharactesInRange(a, b);
+        }
+        static void CharactesInRange(char a, char b)
+        {
+            char newChar = a;
+            if(a > b)
+            {
+                a = b;
+                b = newChar;
+            }
+            for(int i=a+1; i<b; i++) 
+            {
+                char symbol = (char)i;
+                Console.Write(symbol + " ");
+            }
+        }
+    }
+}
