3e64ba7c967499f6e5b171cc63e06f706e33fc49
Gergana Ivanova <geri7423@abv.bg>
2024-02-12 23:50:47 +0200

    new

---
+++ ExerciseMethods/08. Factorial Division/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Numerics;
+
+namespace _08._Factorial_Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());   
+
+            double firstFactoriel = CalculateFactoriel(firstNumber);
+            double secondFactoriel=CalculateFactoriel(secondNumber);
+            double rezult = firstFactoriel / secondFactoriel;
+            Console.WriteLine($"{rezult:F2}");
+
+        }
+        static double CalculateFactoriel(double number)
+        {
+            double factoriel = 1;
+            for (int i = 1; i <= number; i++)
+            {
+                factoriel*= i;
+            }
+            return factoriel;
+        }
+    }
+}
