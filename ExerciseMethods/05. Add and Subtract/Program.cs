50432af2640b6f51b4cfcc977c8fbb80730a5aa2
Gergana Ivanova <geri7423@abv.bg>
2024-02-12 23:48:29 +0200

    new

---
+++ ExerciseMethods/05. Add and Subtract/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Net.Mail;
+
+namespace _05._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int sum = SumNumbers(firstNumber, secondNumber);
+            int substract = SubtractNumbers(sum, thirdNumber);
+
+            Console.WriteLine(substract);
+
+        }
+        static int SumNumbers(int a, int b)
+        {
+            int sum = a + b;
+            return sum;
+        }
+
+        static int SubtractNumbers(int a, int b)
+        {
+            int rezult = a - b;
+            return rezult;
+        }
+    }
+}
