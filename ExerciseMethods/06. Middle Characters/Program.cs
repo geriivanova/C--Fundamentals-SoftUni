3041e5ea17114e5741c303f85d230e3d25cf6e66
Gergana Ivanova <geri7423@abv.bg>
2024-02-12 23:49:37 +0200

    new

---
+++ ExerciseMethods/06. Middle Characters/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _06._Middle_Characters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            MiddleCharacters(word);
+        }
+        
+        static void MiddleCharacters(string characters)
+        {
+            int length = characters.Length;
+            if(length % 2 == 0)
+            {
+                for (int i = 0; i <= length / 2; i++)
+                {
+                    if (i == length / 2)
+                    {
+                        Console.WriteLine($"{characters[i - 1]}{ characters[i]}");
+                    }
+                }
+            }
+            else if( length % 2 != 0)
+            {
+                for (int i = 0; i <= length / 2; i++)
+                {
+                    if (i == length / 2)
+                    {
+                        Console.WriteLine(characters[i]);
+                    }
+                }
+            }
+        }
+    }
+}
