1744fb0d97fd25ec92492daae6ca28a9334cf2b8
Gergana Ivanova <geri7423@abv.bg>
2024-02-13 23:15:49 +0200

    new add

---
+++ ExerciseMethods/09. Palindrome Integers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _09._Palindrome_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            while(number != "END")
+            {
+                IsPalindrom(number);
+                number = Console.ReadLine();
+            }
+            
+        }
+
+        static void IsPalindrom(string number)
+        {
+            string check = "true";
+            for(int i=0; i<number.Length; i++)
+            {
+                if (number[i] != number[number.Length-1 - i])
+                {
+                    check = "false";
+                    break;
+                }
+            }
+            Console.WriteLine(check);
+        }
+    }
+}
