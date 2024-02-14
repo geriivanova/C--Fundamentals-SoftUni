927e8e998587444daf20c7af5767e2619a6e22ad
Gergana Ivanova <geri7423@abv.bg>
2024-02-14 22:39:43 +0200

    add

---
+++ ExerciseLists/02. Change List/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _02._Change_List
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            string commands;
+            while((commands = Console.ReadLine()) != "end")
+            {
+                string[] arguments = commands.Split();
+
+                if (arguments[0] == "Delete") 
+                { 
+                    int newNumber= int.Parse(arguments[1]);
+                    for(int i=0; i<numbers.Count; i++)
+                    {
+                        if (numbers[i] == newNumber)
+                        {
+                            numbers.RemoveAt(i);
+                            i--;
+                        }
+                    }
+                }
+                else if (arguments[0] == "Insert")
+                {
+                    int element = int.Parse(arguments[1]);
+                    int index = int.Parse(arguments[2]);
+
+                    numbers.Insert(index, element);
+                }
+            }
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+    }
+}
