9103c1097fc4d723221fc7829dbd48e8fa4f3f34
Gergana Ivanova <geri7423@abv.bg>
2024-02-14 22:40:08 +0200

    add

---
+++ ExerciseLists/03. House Party/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _03._House_Party
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int guestCount = int.Parse(Console.ReadLine());
+
+            List <string> name = new List<string>();
+
+            for(int i=0; i<guestCount; i++)
+            {
+                string[] commands = Console.ReadLine().Split();
+                string guestName= commands[0];
+                if (commands[2]=="going!")
+                {
+                    if (name.Exists(n => n == guestName))
+                    {
+                        Console.WriteLine($"{guestName} is already in the list!");
+                    }
+                    else
+                    {
+                        name.Add(guestName);
+                    }
+                }
+                else if (commands[2]=="not")
+                {
+                    if (name.Exists(n => n == guestName))
+                    {
+                        name.Remove(guestName);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{guestName} is not in the list!");
+                    }
+                }
+            }
+            Console.WriteLine(string.Join("\n", name));
+        }
+    }
+}
