5096cb0d7649d04cf1a497e0e0b790ffca9e7248
Gergana Ivanova <geri7423@abv.bg>
2024-02-20 22:17:56 +0200

    add

---
+++ LabAssociativeArrays/02. Odd Occurrences/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace _02._Odd_Occurrences
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            Dictionary<string, int> counts= new Dictionary<string, int>();    
+
+            foreach(string word in words)
+            {
+                string wordToLowerCase = word.ToLower();
+
+                if(counts.ContainsKey(wordToLowerCase))
+                {
+                    counts[wordToLowerCase]++;
+                }
+                else
+                {
+                    counts.Add(wordToLowerCase, 1);
+                }
+            }
+
+            foreach(var count in counts)
+            {
+                if(count.Value % 2 != 0)
+                {
+                    Console.Write(count.Key + " ");
+                }
+            }
+        }
+    }
+}
