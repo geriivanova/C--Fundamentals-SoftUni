42c846dee960ec31fee5e37678d20e9c93de5d93
Gergana Ivanova <geri7423@abv.bg>
2024-02-21 21:17:39 +0200

    new add

---
+++ LabTextProcessing/02. Repeat Strings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+namespace _02._Repeat_Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split();
+
+            StringBuilder rezult = new StringBuilder();
+
+            foreach (string word in words)
+            {
+                int count = word.Length;
+
+                for(int i = 0; i < count; i++) 
+                { 
+                    rezult.Append(word);
+                }
+            }
+            Console.WriteLine(rezult);
+        }
+    }
+}
