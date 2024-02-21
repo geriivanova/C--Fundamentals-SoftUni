2264538f656c2d63ed03590eb4a3ecc45ee5c45b
Gergana Ivanova <geri7423@abv.bg>
2024-02-21 21:20:56 +0200

    new add

---
+++ LabTextProcessing/05. Digits, Letters and Other/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace _05._Digits__Letters_and_Other
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            string digits = string.Empty;
+            string letters = string.Empty;
+            string symbols = string.Empty;
+
+            for(int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];   
+                if(char.IsDigit(c))
+                {
+                    digits += c;
+                }
+                else if(char.IsLetter(c))
+                {
+                    letters += c;
+                }
+                else
+                {
+                    symbols += c;
+                }
+            }
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(symbols);
+        }
+    }
+}
