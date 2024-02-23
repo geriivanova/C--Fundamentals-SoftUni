cbedd6ead51e0d0fb24dff18876dff31b819b963
Gergana Ivanova <geri7423@abv.bg>
2024-02-23 22:55:55 +0200

    new

---
+++ ExerciseTextProcessing/01. Valid Usernames/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _01._Valid_Usernames
+{
+    internal class Program
+    {
+        private static bool IsValid(string username)
+        {
+            return username.All(c => char.IsLetterOrDigit(c) || c == '-' || c == '_');
+        }
+
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ");
+            foreach(string username in usernames)
+            {
+                if(username.Length < 3 || username.Length > 16)
+                {
+                    continue;
+                }
+                if(IsValid(username))
+                {
+                    Console.WriteLine(username);
+                }
+            }
+        }
+    }
+}
