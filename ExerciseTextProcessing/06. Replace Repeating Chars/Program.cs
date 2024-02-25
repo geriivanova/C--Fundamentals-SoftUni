c33c195be69de2e2afb942dbe169801ed71cb9f6
Gergana Ivanova <geri7423@abv.bg>
2024-02-25 22:45:22 +0200

    new

---
+++ ExerciseTextProcessing/06. Replace Repeating Chars/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace _06._Replace_Repeating_Chars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder rezult = new StringBuilder();
+            rezult.Append(text[0]);
+
+            for(int i = 1; i < text.Length; i++) 
+            {
+                if (text[i] != text[i-1])
+                {
+                    rezult.Append(text[i]);
+                }
+            }
+            Console.WriteLine(rezult);
+        }
+    }
+}
