e6fdfa8e3bfab1ca7a98c9000377e26d11fecd79
Gergana Ivanova <geri7423@abv.bg>
2024-02-26 20:22:55 +0200

    new add

---
+++ ExerciseTextProcessing/04. Caesar Cipher/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace _04._Caesar_Cipher
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder cipherBuilder = new StringBuilder();
+
+            for(int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                cipherBuilder.Append((char)(c + 3));
+            }
+            Console.WriteLine(cipherBuilder);
+        }
+    }
+}
