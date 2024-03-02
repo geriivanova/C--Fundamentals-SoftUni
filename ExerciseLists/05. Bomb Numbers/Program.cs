9f9726e73ed3aa2114762c96d13d38bf9e7906f2
Gergana Ivanova <geri7423@abv.bg>
2024-03-03 00:38:44 +0200

    new add

---
+++ ExerciseLists/05. Bomb Numbers/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+
+namespace _05._Bomb_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+            List<int >numberAndPower = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            numbers = Explode(numbers, numberAndPower);
+
+            int sum = 0;
+            foreach(int i in numbers)
+            {
+                sum += i;
+            }
+            Console.WriteLine(sum);
+        }
+        private static List<int> Explode(List<int> list, List<int> bomb)
+        {
+            int number = bomb[0];
+            int power = bomb[1];
+
+            while (list.Contains(number))
+            {
+                int index = list.IndexOf(number);
+
+                int leftIndex = Math.Max(0, index - power);
+                int rightIndex = Math.Min(list.Count - 1, index + power);
+
+                int range = rightIndex - leftIndex + 1;
+                list.RemoveRange(leftIndex, range);
+            }
+
+            return list;
+        }
+    }
+}
