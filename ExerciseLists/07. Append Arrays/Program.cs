edf9d8e4a82aa0ba64d9e72c8bd104b843a36dcc
Gergana Ivanova <geri7423@abv.bg>
2024-03-05 00:54:27 +0200

    new add

---
+++ ExerciseLists/07. Append Arrays/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _07._Append_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List <string> arrays = Console.ReadLine()
+                .Split('|', StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            List<string> newList = new List<string>();
+            for (int i = arrays.Count - 1; i >= 0; i--)
+            {
+                string[] symbol = arrays[i]
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+                newList.AddRange(symbol);
+            }
+            Console.WriteLine (string.Join(" ", newList));
+        }
+    }
+}
