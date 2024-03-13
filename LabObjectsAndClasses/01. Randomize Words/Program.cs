c36a3413656323d990aefc280ae35089ca0d9c12
Gergana Ivanova <geri7423@abv.bg>
2024-03-13 16:03:40 +0200

    Update Program.cs

--- LabObjectsAndClasses/01. Randomize Words/Program.cs
+++ LabObjectsAndClasses/01. Randomize Words/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] words = Console.ReadLine().Split();
+            Random rnd = new Random();
+
+            for(int i = 0; i < words.Length; i++)
+            {
+                int pos = rnd.Next(words.Length);
+                string temp = words[i];
+                words[i] = words[pos];
+                words[pos] = temp;
+            }
+            Console.WriteLine(string.Join("\n", words)); 
         }
     }
 }
