fe2dd0d58e322cab3be1968bb7601e5259170920
Gergana Ivanova <geri7423@abv.bg>
2024-03-13 16:09:45 +0200

    finish task

--- LabObjectsAndClasses/02. Big Factorial/Program.cs
+++ LabObjectsAndClasses/02. Big Factorial/Program.cs
@@ -1,10 +1,19 @@
-﻿namespace _02._Big_Factorial
+﻿using System.Numerics;
+
+namespace _02._Big_Factorial
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+            BigInteger factoriel = 1;
+
+            for(int i = 2; i <= n; i++)
+            {
+                factoriel *= i;
+            }
+            Console.WriteLine(factoriel);
         }
     }
 }
